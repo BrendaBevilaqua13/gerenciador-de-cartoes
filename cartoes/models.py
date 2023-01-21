f757fe37f29339f37b410da1ff47a581572b2d3d
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-21 16:06:17 -0300

    migrando modelo de cartao

--- cartoes/models.py
+++ cartoes/models.py
@@ -6,4 +6,4 @@ class Cartao(models.Model):
     titular = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     nome = models.CharField(max_length=15)
     def __str__(self):
-        self.nome
+        return self.nome
