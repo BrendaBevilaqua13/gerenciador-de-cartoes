c874cf90e5fca285a60132a3ce73ee4eecfe23a2
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-21 15:41:18 -0300

    criando modelo de cartao

--- cartoes/models.py
+++ cartoes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cartao(models.Model):
+    titular = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    nome = models.CharField(max_length=15)
+    def __str__(self):
+        self.nome
