96545a73d7f9c6ad9019523f92ecc23da9a82502
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-21 17:24:54 -0300

    criando template e rota de cartoes e vinculando a um usuario

--- cartoes/views.py
+++ cartoes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from . models import Cartao
 
 
 def home(request):
-    return render(request, 'cartoes/pages/home.html', {'cartao': 'nubank'})
+    return render(request, 'cartoes/pages/home.html')
+
+
+def cartoes(request):
+    usuario_cartoes = Cartao.objects.filter(titular=request.user.id)
+    cartoes  = {
+        'cartoes': usuario_cartoes,
+    }
+    return render(request, 'cartoes/pages/cartoes.html', cartoes)
 
 
 
