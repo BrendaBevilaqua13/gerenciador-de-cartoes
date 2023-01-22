b9749e2eb244f13805f91cf5ad2b7dd7eb7d8923
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-22 17:46:03 -0300

    buscando cartoes filtrando pelo nome da pessoa e mostrando no template home

--- cartoes/views.py
+++ cartoes/views.py
@@ -4,9 +4,10 @@ from pessoas.models import Pessoa
 
 
 def home(request):
-
-    return render(request, 'cartoes/pages/home.html')
-
-
-def cartoes(request):
-    pass
+    pessoa = Pessoa.objects.get(nome='João')
+    cartoes = Cartao.objects.filter(titular=pessoa)
+    cartoes_buscados = {
+        'cartoes': cartoes,
+        'pessoa': pessoa
+    }
+    return render(request, 'cartoes/pages/home.html', cartoes_buscados)
