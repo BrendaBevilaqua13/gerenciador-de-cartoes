42e6090c492d9025c622a12fa6316cbfe2d94496
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-22 16:35:43 -0300

    criando app pessoas e criando usuarios

--- cartoes/models.py
+++ cartoes/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from pessoas.models import Pessoa
 
 
 class Cartao(models.Model):
-    titular = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    titular = models.ForeignKey(Pessoa, on_delete=models.CASCADE, null=True, blank=True)
     nome = models.CharField(max_length=15)
     def __str__(self):
         return self.nome
--- cartoes/views.py
+++ cartoes/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render, HttpResponse
-from . models import Cartao
+from django.shortcuts import render
+from .models import Cartao
+from pessoas.models import Pessoa
 
 
 def home(request):
+
     return render(request, 'cartoes/pages/home.html')
 
 
 def cartoes(request):
-    usuario_cartoes = Cartao.objects.filter(titular=request.user.id)
-    cartoes  = {
-        'cartoes': usuario_cartoes,
-    }
-    return render(request, 'cartoes/pages/cartoes.html', cartoes)
-
-
-
+    pass
